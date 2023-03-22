68a52635e1955a512531667f3db974a516fbbe4e
Matija Rezek <matija.rezek@hotmail.com>
2023-03-22 20:00:02 +0200

    Prvi committ

---
+++ libs/Boot.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core;
+
+use Symfony\Component\Dotenv\Dotenv;
+
+class Boot
+{
+    public function boot()
+    {
+        // parser za .env fileove
+
+        $envParaser = new Dotenv();
+        $envParaser->loadEnv(ROOT . '/.env');
+
+        // spremanje podataka za povezivanje u konstante
+        define('DB_DSN', $_ENV['DB_DSN']);
+        define('DB_USERNAME', $_ENV['DB_USERNAME']);
+        define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+    }
+}
